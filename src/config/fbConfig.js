93da2f0c36ce8080315acbc35539fdcb7539af93
Bryan Hunter <bdhunter3141@gmail.com>
2018-11-21 15:29:12 -0500

    connected to firebase and posting projects to database.

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAJ6WSIJjFsZKAW-Au5Cdxjv337ckDVUPc",
+  authDomain: "naco-employee-portal.firebaseapp.com",
+  databaseURL: "https://naco-employee-portal.firebaseio.com",
+  projectId: "naco-employee-portal",
+  storageBucket: "naco-employee-portal.appspot.com",
+  messagingSenderId: "18841270613"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;

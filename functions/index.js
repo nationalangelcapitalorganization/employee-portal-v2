040322c422d17ab7b002deba5ba6e85c0cac9e31
Bryan Hunter <bdhunter3141@gmail.com>
2018-12-14 14:16:22 -0500

    figuring out image uploads.

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
+const fetch = require("isomorphic-fetch");
+const cloudname = functions.config().cloudinary.cloudname
+const uploadPreset = functions.config().cloudinary.uploadpreset
 
 
 const createNotification = (notification => {
@@ -79,7 +81,29 @@ exports.userJoined = functions.auth.user()
 
 
 exports.imageUpload = functions.https.onRequest((request, response) => {
-  console.log('this is a request', request.body)
-  response.set('Access-Control-Allow-Origin', '*')
-  response.send(JSON.stringify({ location: 'https://d3lut3gzcpx87s.cloudfront.net/image_encoded/aHR0cHM6Ly9zaWxrc3RhcnQuczMuYW1hem9uYXdzLmNvbS83MThkNmY1OC00NzI1LTQzNmEtYTcyZi03M2EzYzc0ZDJkM2QucG5n/540x100fPNG' }));
+
+
+  postData(`https://api.cloudinary.com/v1_1/${cloudname}/image/upload`, 
+  { file: request.body, 
+    upload_preset: uploadPreset
+  }).then(data => console.log(JSON.stringify(data)))
+    .catch(error => console.error(error));
+
+  function postData(url = ``, data = {}) {
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json()); 
+  }
+
+
+  let data = {
+    location: 'https://d3lut3gzcpx87s.cloudfront.net/image_encoded/aHR0cHM6Ly9zaWxrc3RhcnQuczMuYW1hem9uYXdzLmNvbS83MThkNmY1OC00NzI1LTQzNmEtYTcyZi03M2EzYzc0ZDJkM2QucG5n/540x100fPNG'
+  }
+    response.set('Access-Control-Allow-Origin', '*')
+    response.send(JSON.stringify(data))
 });
